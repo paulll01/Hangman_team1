f62db442657fb37105c0c4e883254f800af72e87
paullll01 <pitispaul46@gmail.com>
2025-03-22 17:09:47 +0000

    Initial Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Aici este fisierul principal unde vom asabla toate modulele si vom rula aplicatia
+
+# Pasul 1 - Introducere cuvinte de ghicit 
+# Pasul 2 - Clasificarea cuvintelor pe niveluri de dificultate si numarul de greseli acceptate
+# Pasul 2.1 - Extragerea unui cuvant random din lista de cuvinte
+# Pasul 3 - Secventiere/afisare grafica a spanzuratorii in functie de numarul de greseli
+# Pasul 4 - Citire de la tastatura a literei introduse de jucator
+# Pasul 5 - Cautarea in structura a caracterului introdus de jucator si validare literei
+# Pasul 6 - Actualizare grafica a spanzuratorii in functie de numarul de greseli
+# Pasul 7 - Reluare ciclu daca mai sunt permise incercari
+# Pasul 8 - Reluare ciclu daca cuvantul nu a fost ghicit
+# Pasul 9 - Crearea unei formule de performanta pentru a fi introdusa intr-un clasament
