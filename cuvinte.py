3109d4a12dff719c961b21b6704eb7ee21d31397
Paul Pitis <73071669+paulll01@users.noreply.github.com>
2025-03-25 21:11:41 +0200

    Create cuvinte.py
    Daniel

---
+++ cuvinte.py
@@ -0,0 +1,11 @@
+# Daniel
+
+# Cerem o litera de la tastatura
+def cere_litera(litere_incercate):
+
+# aici incarcam o lista de c=uvinte din fisier din .txt -> Majuscule/fara diacritice
+def incarca_cuvinte(): 
+
+# trimitem lista de cuvinte si cu random returneaza un cuvant random
+def alege_cuvant(cuvinte):
+
